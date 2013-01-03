T-J-Teru/browse-kill-ring

commit 791330f708a01bcad74bf99f4708773cb8721dae

gitignore, merged some fixes from another branch and made in so we can paste over and over!!

browse-kill-ring.el
@@ -5,8 +5,8 @@
 ;; Author: Colin Walters <walters@verbum.org>
 ;; Maintainer: Nick Hurley <hurley@cis.ohio-state.edu>
 ;; Created: 7 Apr 2001
-;; Version: 1.3 (CVS)
-;; X-RCS: $Id: browse-kill-ring.el,v 1.2 2002/10/29 00:23:00 hurley Exp $
+;; Version: 1.3c
+;; X-RCS: $Id: browse-kill-ring.el,v 1.2 2008/10/29 00:23:00 hurley Exp $
 ;; URL: http://freedom.cis.ohio-state.edu/~hurley/
 ;; URL-ja: http://www.fan.gr.jp/~ring/doc/browse-kill-ring.html
 ;; Keywords: convenience
@@ -49,6 +49,24 @@
 ;; again.
 
 ;;; Change Log:
+
+;; Changes from 1.3b to 1.3c:
+
+;; * 28-Feb-2011: Andrew Burgess <aburgess@broadcom.com>
+;;   Fix a bug where having other overlays active in the kill ring buffer,
+;;   for example with show-paren-mode would block insertion.
+
+;; Changes from 1.3a to 1.3b:
+
+;; * 24-Feb-2011: Andrew Burgess <aburgess@broadcom.com>
+;;   Correctly handle inserting when multiple windows exist for the
+;;   same buffer.
+
+;; Changes from 1.3 to 1.3a:
+
+;; * Sneak update by Benjamin Andresen <bandresen@gmail.com>
+;; * Added the read-only bugfix (http://bugs.debian.org/225082) from 
+;;   the emacs-goodies-el package
 
 ;; Changes from 1.2 to 1.3:
 
@@ -337,7 +355,7 @@ when you call `kill-new'.
 If you set this variable via customize, the advice will be activated
 or deactivated automatically.  Otherwise, to enable the advice, add
 
- (ad-enable-advice 'kill-new 'around 'browse-kill-ring-no-kill-new-duplicates)
+B (ad-enable-advice 'kill-new 'around 'browse-kill-ring-no-kill-new-duplicates)
  (ad-activate 'kill-new)
 
 to your init file."
@@ -605,33 +623,33 @@ of the *Kill Ring*."
   (browse-kill-ring-resize-window)
   (browse-kill-ring-forward 0))
 
+;; Helper function for browse-kill-ring-current-string, takes a list of
+;; overlays and returns the string from the first overlay that has the 
+;; property. There might be more than just our overlay at this point.
+(defun browse-kill-ring-current-string-1 (overs)
+  (if overs
+      (let ((str (overlay-get (car overs) 'browse-kill-ring-target)))
+        (if str str (browse-kill-ring-current-string-1 (cdr overs))))
+    nil))
+
+;; Find the string to insert at the point by looking for the overlay.
 (defun browse-kill-ring-current-string (buf pt)
-  (with-current-buffer buf
-    (let ((overs (overlays-at pt)))
-      (or (and overs
-	       (overlay-get (car overs) 'browse-kill-ring-target))
- 	  (error "No kill ring item here")))))
+  (or (browse-kill-ring-current-string-1 (overlays-at pt))
+      (error "No kill ring item here")))
 
 (defun browse-kill-ring-do-insert (buf pt)
   (let ((str (browse-kill-ring-current-string buf pt)))
-    (let ((orig (current-buffer)))
-      (unwind-protect
-	  (progn
-	    (unless (window-live-p browse-kill-ring-original-window)
-	      (error "Window %s has been deleted; Try calling `browse-kill-ring' again"
-		     browse-kill-ring-original-window))
-	    (set-buffer (window-buffer browse-kill-ring-original-window))
-	    (save-excursion
-	      (let ((pt (point)))
-		(insert (if browse-kill-ring-depropertize
-			    (browse-kill-ring-depropertize-string str)
-			  str))
-		(when browse-kill-ring-highlight-inserted-item
-		  (let ((o (make-overlay pt (point))))
-		    (overlay-put o 'face 'highlight)
-		    (sit-for 0.5)
-		    (delete-overlay o))))))
-	(set-buffer orig)))))
+    (save-selected-window
+      (select-window browse-kill-ring-original-window)
+
+      (let (deactivate-mark)
+	(insert-for-yank str))
+      
+      (when browse-kill-ring-highlight-inserted-item
+	(let ((o (make-overlay pt (point))))
+	  (overlay-put o 'face 'highlight)
+	  (sit-for 0.5)
+	  (delete-overlay o))))))
 
 (defun browse-kill-ring-forward (&optional arg)
   "Move forward by ARG `kill-ring' entries."
@@ -899,13 +917,19 @@ directly; use `browse-kill-ring' instead.
   (let* ((item (browse-kill-ring-elide origitem))
 	 (len (length item)))
     (browse-kill-ring-add-overlays-for origitem
-	(insert item))
-    (insert "\n")
-    (when separatep
-      (insert (browse-kill-ring-propertize browse-kill-ring-separator
-					   'browse-kill-ring-extra t
-					   'browse-kill-ring-separator t))
-      (insert "\n"))))
+                                       (insert item))
+    ;; When the kill-ring has items with read-only text property at
+    ;; **the end of** string, browse-kill-ring-setup fails with error
+    ;; `Text is read-only'.  So inhibit-read-only here.
+    ;; See http://bugs.debian.org/225082
+    ;; - INOUE Hiroyuki <dombly@kc4.so-net.ne.jp>
+    (let ((inhibit-read-only t))
+      (insert "\n")
+      (when separatep
+        (insert (browse-kill-ring-propertize browse-kill-ring-separator
+                                             'browse-kill-ring-extra t
+                                             'browse-kill-ring-separator t))
+        (insert "\n")))))
 
 (defun browse-kill-ring-occur (regexp)
   "Display all `kill-ring' entries matching REGEXP."
@@ -1031,7 +1055,7 @@ directly; use `browse-kill-ring' instead.
       (browse-kill-ring-setup buf (selected-window))
       (pop-to-buffer buf)
       (browse-kill-ring-resize-window)
-      nil)))
+      )))
 
 (provide 'browse-kill-ring)
 

.gitignore
@@ -1,0 +1,1 @@
+*.elc

commit 3ea1691713c05ca0c75408abfedceb8e5d7dc3fc

Fix bug with highlighting inserted item
791330f7 introduced a bug by removing a redefinition of the variable
pt. pt as passed in is the value of point in the Kill Ring buffer; it
bears no relationship whatsoever to the value we want, which is the
value of point in the buffer we're inserting into.

Make this a little clearer by using a new name for the variable. This
requires a reindent.

browse-kill-ring.el
@@ -669,15 +669,16 @@ of the *Kill Ring*."
 (defun browse-kill-ring-do-insert (buf pt)
   (let ((str (browse-kill-ring-current-string buf pt)))
     (with-current-buffer browse-kill-ring-original-buffer
+      (let ((before-insert (point)))
 
-      (let (deactivate-mark)
-        (insert-for-yank str))
+        (let (deactivate-mark)
+          (insert-for-yank str))
 
-      (when browse-kill-ring-highlight-inserted-item
-        (let ((o (make-overlay pt (point))))
-          (overlay-put o 'face browse-kill-ring-inserted-item-face)
-          (sit-for 0.5)
-          (delete-overlay o))))))
+        (when browse-kill-ring-highlight-inserted-item
+          (let ((o (make-overlay before-insert (point))))
+            (overlay-put o 'face browse-kill-ring-inserted-item-face)
+            (sit-for 0.5)
+            (delete-overlay o)))))))
 
 (defun browse-kill-ring-forward (&optional arg)
   "Move forward by ARG `kill-ring' entries."