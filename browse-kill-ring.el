T-J-Teru/browse-kill-ring

commit ca0b5f44c6c46b7d376a9634ffb49a4f83963ea9

Teach browse-kill-ring to insert into a specific buffer, not window. This fixes interop issues with popwin.el

browse-kill-ring.el
@@ -404,6 +404,11 @@ inserted with properties."
 It is probably not a good idea to set this variable directly; simply
 call `browse-kill-ring' again.")
 
+(defvar browse-kill-ring-original-buffer nil
+  "The buffer in which chosen kill ring data will be inserted.
+It is probably not a good idea to set this variable directly; simply
+call `browse-kill-ring' again.")
+
 (defun browse-kill-ring-mouse-insert (e)
   "Insert the chosen text, and close the *Kill Ring* buffer afterwards."
   (interactive "e")
@@ -651,7 +656,7 @@ of the *Kill Ring*."
 
 (defun browse-kill-ring-do-insert (buf pt)
   (let ((str (browse-kill-ring-current-string buf pt)))
-    (with-selected-window browse-kill-ring-original-window
+    (with-current-buffer browse-kill-ring-original-buffer
 
       (let (deactivate-mark)
         (insert-for-yank str))
@@ -984,8 +989,8 @@ directly; use `browse-kill-ring' instead.
                           browse-kill-ring-original-window)
   (browse-kill-ring-resize-window))
 
-(defun browse-kill-ring-setup (buf window &optional regexp window-config)
-  (with-current-buffer buf
+(defun browse-kill-ring-setup (kill-buf orig-buf window &optional regexp window-config)
+  (with-current-buffer kill-buf
     (unwind-protect
         (progn
           (browse-kill-ring-mode)
@@ -995,7 +1000,8 @@ directly; use `browse-kill-ring' instead.
             (setq truncate-lines t))
           (let ((inhibit-read-only t))
             (erase-buffer))
-          (setq browse-kill-ring-original-window window
+          (setq browse-kill-ring-original-buffer orig-buf
+                browse-kill-ring-original-window window
                 browse-kill-ring-original-window-config
                 (or window-config
                     (current-window-configuration)))
@@ -1061,8 +1067,10 @@ directly; use `browse-kill-ring' instead.
   (interactive)
   (if (eq major-mode 'browse-kill-ring-mode)
       (message "Already viewing the kill ring")
-    (let ((buf (get-buffer-create "*Kill Ring*")))
-      (browse-kill-ring-setup buf (selected-window))
+    (let* ((orig-win (selected-window))
+           (orig-buf (window-buffer orig-win))
+           (buf (get-buffer-create "*Kill Ring*")))
+      (browse-kill-ring-setup buf orig-buf orig-win)
       (pop-to-buffer buf)
       (browse-kill-ring-resize-window))))
 

commit 6a95c14d6692e441c437ed8cfaca100397f3252f

Fix browse-kill-ring-update
Commit ca0b5f4 broke this function by updating the signature of
browse-kill-ring-setup, but not updating it everywhere.

Additionally, calling browse-kill-ring-setup multiple times leads to
duplicating overlays, so fix that too.

This fixes #15.

browse-kill-ring.el
@@ -50,6 +50,10 @@
 
 ;; Changes from 1.4 to 1.5:
 
+;; * 2013-Aug-10: Ethan Glasser-Camp
+;;   Fix browse-kill-ring-update. Commit ca0b5f4 broke it. Fixing it
+;;   also exposed some problems with how the overlay is handled.
+
 ;; * 2013-Jul-29: Ethan Glasser-Camp
 ;;   Make `browse-kill-ring-quit-action' default to
 ;;   `save-and-restore'. This seems to DWIM in most cases: running
@@ -1041,6 +1045,7 @@ directly; use `browse-kill-ring' instead.
   (interactive)
   (assert (eq major-mode 'browse-kill-ring-mode))
   (browse-kill-ring-setup (current-buffer)
+                          browse-kill-ring-original-buffer
                           browse-kill-ring-original-window)
   (browse-kill-ring-resize-window))
 
@@ -1070,8 +1075,10 @@ directly; use `browse-kill-ring' instead.
                   (max (point) (mark))
                 (point))))
     (when browse-kill-ring-show-preview
+      (when browse-kill-ring-preview-overlay
+        (delete-overlay browse-kill-ring-preview-overlay))
       (setq browse-kill-ring-preview-overlay
-            (make-overlay start end (current-buffer)))))
+            (make-overlay start end orig-buf))))
   (overlay-put browse-kill-ring-preview-overlay
                'invisible t)
   (with-current-buffer kill-buf